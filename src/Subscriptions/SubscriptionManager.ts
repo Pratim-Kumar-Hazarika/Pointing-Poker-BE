3360df6c73c62d6fee49490bb11385b4044eaf9b
Pratim <hpro373@gmail.com>
2024-09-05 01:49:20 +0530

    Update SubscriptionManager.ts

--- src/Subscriptions/SubscriptionManager.ts
+++ src/Subscriptions/SubscriptionManager.ts
@@ -22,9 +22,36 @@ export class SubscriptionManager{
         return this.instance;
     }
 
-    public subscribe(userId:string,subscriptionId:string){}
+    public subscribe(userId:string,subscriptionId:string){
+        if(this.subscriptions.get(userId)?.includes(subscriptionId)){
+            return;
+        }
 
-    public unsubscribe(userId:string,subscriptionId:string){}
+        this.subscriptions.set(userId,(this.subscriptions.get(userId) || []).concat(subscriptionId));
+        this.reverseSubscriptions.set(subscriptionId,(this.reverseSubscriptions.get(subscriptionId) || []).concat(userId));
+
+        if(this.reverseSubscriptions.get(subscriptionId)?.length===1){
+            this.redisClient.subscribe(subscriptionId,this.redisCallBackHandler)
+        }
+
+    }
+
+    public unsubscribe(userId:string,subscriptionId:string){
+        const subscriptions = this.subscriptions.get(userId);
+        if(subscriptions){
+            this.subscriptions.set(userId,subscriptions.filter(s=> s!==subscriptionId))
+        }
+        const reverseSubscriptions = this.reverseSubscriptions.get(subscriptionId);
+        if(reverseSubscriptions){
+            this.reverseSubscriptions.set(subscriptionId, reverseSubscriptions.filter(s=> s!==userId))
+
+            if(this.reverseSubscriptions.get(subscriptionId)?.length ===0){
+                this.reverseSubscriptions.delete(subscriptionId)
+                this.redisClient.unsubscribe(subscriptionId)
+            }
+        }
+
+    }
 
     private redisCallBackHandler(message:string,channelId:string){
         const parsedMessage = JSON.parse(message);
