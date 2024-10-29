3e46b2799d97732f455f6ff734fd147e15d0a3dd
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2024-10-30 00:13:40 +0530

    Update queries.ts

--- src/postgress/queries.ts
+++ src/postgress/queries.ts
@@ -1,9 +1,36 @@
+import { pool } from "../Db/Db";
 
-export async function handleTotalVotes(message: string) {
-    console.log("Total votes incremented");
+export async function incrementUsers() {
+  const client = await pool.connect();
+  try {
+    await client.query('INSERT INTO total_users (count) VALUES (0)');
+    console.log("New row added to total_users with count 0");
+  } catch (error) {
+    console.error("Error inserting new row in total_users:", error);
+  } finally {
+    client.release();
   }
-  
-  export async function handleTotalPlayers(message: string) {
-    console.log("Total players incremented");
+}
+export async function incrementVotes() {
+  const client = await pool.connect();
+  try {
+    await client.query('INSERT INTO total_votes (count) VALUES (0)');
+    console.log("New row added to total_votes with count 0");
+  } catch (error) {
+    console.error("Error inserting new row in total_votes:", error);
+  } finally {
+    client.release();
   }
-  
+}
+
+export async function incrementSessions() {
+  const client = await pool.connect();
+  try {
+    await client.query('INSERT INTO total_sessions (count) VALUES (0)');
+    console.log("New row added to total_sessions with count 0");
+  } catch (error) {
+    console.error("Error inserting new row in total_sessions:", error);
+  } finally {
+    client.release();
+  }
+}
